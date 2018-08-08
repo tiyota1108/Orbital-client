bf60c21fe9d96744ae57cc3312d126a32df93b4f
tiyota1108 <31646540+tiyota1108@users.noreply.github.com>
2018-08-08 16:34:32 +0800

    remove changes in notes icon

--- src/Notes.js
+++ src/Notes.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import Card from './Card'
-import FaPlus from 'react-icons/lib/fa/plus-square'
+import FaPlus from 'react-icons/lib/fa/plus'
 import FaPencil from 'react-icons/lib/fa/pencil'
-import FaTrash from 'react-icons/lib/fa/trash-alt'
+import FaTrash from 'react-icons/lib/fa/trash'
 import FaFloppyO from 'react-icons/lib/fa/floppy-o'
 import './notes.css'
 //in this version i moved the set state for updating and deleting
